1dd9577d1d188e5cab5cbcd898447496abeebbac
Антон <anton-kulish15@mail.ru>
2025-02-10 12:56:09 +0700

    1 com

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Windows.Forms;
+namespace Lab1_Aoci_Andrew_Anton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(204, 228);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Пикча";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(249, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(208, 228);
+            this.imageBox2.TabIndex = 4;
+            this.imageBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Видео";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(463, 100);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(194, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 80;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(463, 151);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(194, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 40;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1342, 553);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Button button2;
+        private Timer timer1;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.Superres;
+
+namespace Lab1_Aoci_Andrew_Anton
+{
+    public partial class Form1 : Form
+    {
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private Image<Bgr, byte> sourceImage;
+        private VideoCapture capture;
+
+        public string fileSelected;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                sourceImage = new Image<Bgr, byte>(fileName);
+            }
+
+            imageBox1.Image = sourceImage.Resize(640, 480, Inter.Linear);
+
+            imageUpdateThres();
+
+            fileSelected = "image";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                capture = new VideoCapture(fileName);
+            }
+
+            fileSelected = "video";
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            videoUpdate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (fileSelected == "image")
+                imageUpdateThres();
+            if (fileSelected == "video")
+                videoUpdate();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (fileSelected == "image")
+                imageUpdateThres();
+            if (fileSelected == "video")
+                videoUpdate();
+        }
+
+        private void imageUpdateThres()
+        {
+            Image<Gray, byte> grayImage = sourceImage.Convert<Gray, byte>();
+
+            var tempImage = grayImage.PyrDown();
+            var destImage = tempImage.PyrUp();
+
+            //double cannyThreshold = 80.0;
+            //double cannyThresholdLinking = 40.0;
+            double cannyThreshold = trackBar1.Value;
+            double cannyThresholdLinking = trackBar2.Value;
+            Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+
+            var cannyEdgesBgr = cannyEdges.Convert<Bgr, byte>();
+            var resultImage = sourceImage.Sub(cannyEdgesBgr);
+
+            for (int channel = 0; channel < resultImage.NumberOfChannels; channel++)
+                for (int x = 0; x < resultImage.Width; x++)
+                    for (int y = 0; y < resultImage.Height; y++)
+                    {
+                        byte color = resultImage.Data[y, x, channel];
+                        if (color <= 50)
+                            color = 0;
+                        else if (color <= 100)
+                            color = 25;
+                        else if (color <= 150)
+                            color = 180;
+                        else if (color <= 200)
+                            color = 210;
+                        else
+                            color = 255;
+
+                        resultImage.Data[y, x, channel] = color;
+                    }
+            imageBox2.Image = resultImage;
+        }
+
+        private void videoUpdate()
+        {
+            var frame = capture.QueryFrame();
+
+            imageBox1.Image = frame;
+
+            Image<Bgr, byte> image = frame.ToImage<Bgr, byte>();
+            image = image.Resize(640, 480, Inter.Linear);               //мастхэв
+
+            Image<Gray, byte> grayImage = image.Convert<Gray, byte>();
+
+            var tempImage = grayImage.PyrDown();
+            var destImage = tempImage.PyrDown();
+
+            //double cannyThreshold = 80.0;
+            //double cannyThresholdLinking = 40.0;
+            double cannyThreshold = trackBar1.Value;
+            double cannyThresholdLinking = trackBar2.Value;
+            Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+
+            var cannyEdgesBgr = cannyEdges.Convert<Bgr, byte>();
+            cannyEdgesBgr = cannyEdgesBgr.Resize(640, 480, Inter.Linear); //главное решение
+            var resultImage = image.Sub(cannyEdgesBgr);
+
+            for (int channel = 0; channel < resultImage.NumberOfChannels; channel++)
+                for (int x = 0; x < resultImage.Width; x++)
+                    for (int y = 0; y < resultImage.Height; y++)
+                    {
+                        byte color = resultImage.Data[y, x, channel];
+                        if (color <= 50)
+                            color = 0;
+                        else if (color <= 100)
+                            color = 25;
+                        else if (color <= 150)
+                            color = 180;
+                        else if (color <= 200)
+                            color = 210;
+                        else
+                            color = 255;
+
+                        resultImage.Data[y, x, channel] = color;
+                    }
+
+            imageBox2.Image = resultImage;
+        }
+    }
+}
